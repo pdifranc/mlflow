fd8701c442882dfc772016a2c05f23390cc7f77c
Paolo Di Francesco <frpaolo@amazon.at>
2022-08-24 14:54:07 +0000

    Add Cognito authentication

--- mlflow/server/js/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ mlflow/server/js/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "jsifawz56093303": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
